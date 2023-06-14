2ee687b30b723aca22373955f878bf6108964259
candybharath <126889025+candybharath@users.noreply.github.com>
2023-06-14 17:36:58 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ class Translation(object):
     ERROR = "<b>ERROR :</b> {}"
 
     START_TEXT = """Hi {},
-I'm URL X Uploader!
+I'm URL Uploader!
 You can upload HTTP/HTTPS direct link, Using this bot!
 
 /help for more details!"""
